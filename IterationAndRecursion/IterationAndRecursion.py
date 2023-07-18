9380562f8aded02974f0d26d3ab2e038e2d3cebf
username <useremail>
2023-07-17 19:54:20 -0700

    Updated

--- IterationAndRecursion/IterationAndRecursion.py
+++ IterationAndRecursion/IterationAndRecursion.py
@@ -1,21 +1,30 @@
 # Samirah Ali, Course: 261, Lab Title: IterationAndRecursion
 
-def factorial_iterative( n ):
-    factorial_iterative = 5
-    result = 1
-    for i in range(n) :
-        result *= i + 1
-    return result
- 
-for i in range (5):
-   print ('Factorial results using an interative function')
-   print ('0! =', factorial_iterative(0))
-   print ('5! =', factorial_iterative(5))
-   print ('10! =', factorial_iterative(10))
-   print ('25! =', factorial_iterative(25))
-   print ('50! =  ', factorial_iterative(50))
-   print ('100! = ', factorial_iterative(100))
+def factorial_iterative(num):
+    fact = 1
+    for number in range(2, num + 1):
+        fact = number * fact
+    return fact
 
+def factorial_recursive(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num * factorial_recursive(num - 1)
 
+def main():
+    print("Factorial results using an iterative function")
+    print("0!", factorial_iterative(0))
+    print("5!", factorial_iterative(5))
+    print("10!", factorial_iterative(10))
+    print("25!", factorial_iterative(25))
+    print("50!", factorial_iterative(50))
+    print("100!", factorial_iterative(100))
 
-
+    print("Factorial results using a recursive function")
+    print("0!", factorial_recursive(0))
+    print("5!", factorial_recursive(5))
+    print("10!", factorial_recursive(10))
+    print("25!", factorial_recursive(25))
+    print("50!", factorial_recursive(50))
+    print("100!", factorial_recursive(100))
